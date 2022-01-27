4d69820fb1719867b7c4a702fbb12aa0c252e716
EngMario97 <eng.mec.mario.rocha@gmail.com>
2022-01-27 17:27:30 -0300

    add_orders 1.1

--- src/shared/infra/http/routes/index.routes.ts
+++ src/shared/infra/http/routes/index.routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import clientsRoutes from "../../../../modules/clients/infra/http/routes/clients.routes";
 import categoriesRoutes from "../../../../modules/categories/infra/http/routes/categories.routes";
 import productsRoutes from "../../../../modules/products/infra/http/routes/products.routes";
+import ordersRoutes from "../../../../modules/orders/infra/http/routes/orders.routes";
+
 
 const routes = Router();
 
@@ -10,5 +12,7 @@ routes.use("/clientes", clientsRoutes);
 routes.use("/categorias", categoriesRoutes);
 
 routes.use("/produtos", productsRoutes);
+
+routes.use("/pedidos", ordersRoutes);
 
 export default routes;
